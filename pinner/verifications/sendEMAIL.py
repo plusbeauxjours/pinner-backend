aacc90a80281fcaf074ca878cedf665bf3022e55
plusbeauxjours <plusbeauxjours@gmail.com>
2019-08-24 17:48:02 +0900

    sendEmail to

--- pinner/verifications/sendEMAIL.py
+++ pinner/verifications/sendEMAIL.py
@@ -6,7 +6,7 @@ from django.template.loader import render_to_string
 
 def sendVerificationEMAIL(to, key):
     subject = 'Verify Your Email🔒'
-    to = ['plusbeauxjours.home@gmail.com']
+    to = [to, ]
     key = 'https://5d60dc9507a4170008abea29--pinner.netlify.com/verification/{}'.format(key)
     ctx = {'key': key}
     msg_html = render_to_string('account/email_confirm.html', ctx)
@@ -16,7 +16,7 @@ def sendVerificationEMAIL(to, key):
 
 def sendConfirmEMAIL(to, key):
     subject = 'Verify Your Email🔒'
-    to = ['plusbeauxjours.home@gmail.com']
+    to = [to, ]
     key = 'https://5d60dc9507a4170008abea29--pinner.netlify.com/confirm/{}'.format(key)
     ctx = {'key': key}
     msg_html = render_to_string('account/email_confirm.html', ctx)
