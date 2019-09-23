8bc039f0596dc1b57b4e5fa357e92256dba200f5
plusbeauxjours <plusbeauxjours@gmail.com>
2019-09-23 12:44:35 +0900

    createThumbnail for locations

--- pinner/locations/queries.py
+++ pinner/locations/queries.py
@@ -29,18 +29,22 @@ def resolve_header(self, info, **kwargs):
 
     cities = models.City.objects.all().order_by('-id')
     for i in cities:
-        gp = i.city_photo.split("?")[0]
-        ThumbnailURL = gp+"?ixlib=rb-0.3.5&q=100&fm=jpg&crop=entropy&cs=tinysrgb&w=80&fit=max"
-        i.city_thumbnail = ThumbnailURL
-        i.save()
-    countries = models.City.objects.all()
+        try:
+            gp = i.city_photo.split("?")[0]
+            ThumbnailURL = gp+"?ixlib=rb-0.3.5&q=100&fm=jpg&crop=entropy&cs=tinysrgb&w=80&fit=max"
+            i.city_thumbnail = ThumbnailURL
+            i.save()
+        except:
+            i.city_thumbnail = None
+            i.save()
+    countries = models.Country.objects.all()
     for i in countries:
         gp = i.country_photo.split("?")[0]
         ThumbnailURL = gp+"?ixlib=rb-0.3.5&q=100&fm=jpg&crop=entropy&cs=tinysrgb&w=80&fit=max"
         i.country_thumbnail = ThumbnailURL
         i.save()
 
-    continents = models.City.objects.all()
+    continents = models.Continent.objects.all()
     for i in continents:
         gp = i.continent_photo.split("?")[0]
         ThumbnailURL = gp+"?ixlib=rb-0.3.5&q=100&fm=jpg&crop=entropy&cs=tinysrgb&w=80&fit=max"
