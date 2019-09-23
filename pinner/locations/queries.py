3597ab7a79d9e826a9b6097da8fef08df0cf7eb7
plusbeauxjours <plusbeauxjours@gmail.com>
2019-09-23 12:38:13 +0900

    createThumbnail for locations

--- pinner/locations/queries.py
+++ pinner/locations/queries.py
@@ -27,6 +27,26 @@ def resolve_header(self, info, **kwargs):
 
     city = models.City.objects.get(city_id=cityId)
 
+    cities = models.City.objects.all().order_by('-id')
+    for i in cities:
+        gp = i.city_photo.split("?")[0]
+        ThumbnailURL = gp+"?ixlib=rb-0.3.5&q=100&fm=jpg&crop=entropy&cs=tinysrgb&w=80&fit=max"
+        i.city_thumbnail = ThumbnailURL
+        i.save()
+    countries = models.City.objects.all()
+    for i in countries:
+        gp = i.country_photo.split("?")[0]
+        ThumbnailURL = gp+"?ixlib=rb-0.3.5&q=100&fm=jpg&crop=entropy&cs=tinysrgb&w=80&fit=max"
+        i.country_thumbnail = ThumbnailURL
+        i.save()
+
+    continents = models.City.objects.all()
+    for i in continents:
+        gp = i.continent_photo.split("?")[0]
+        ThumbnailURL = gp+"?ixlib=rb-0.3.5&q=100&fm=jpg&crop=entropy&cs=tinysrgb&w=80&fit=max"
+        i.continent_thumbnail = ThumbnailURL
+        i.save()
+
     return types.HeaderResponse(city=city)
 
 
