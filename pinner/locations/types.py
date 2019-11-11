4ed3e5960097369593b8abaa6c0098ee7b514c71
plusbeauxjours <plusbeauxjours@gmail.com>
2019-11-11 20:21:24 +0900

    commit to heroku

--- pinner/locations/types.py
+++ pinner/locations/types.py
@@ -130,6 +130,8 @@ class ContinentsResponse(graphene.ObjectType):
 
 
 class TripResponse(graphene.ObjectType):
+    page = graphene.Int()
+    hasNextPage = graphene.Boolean()
     trip = graphene.List(notification_types.MoveNotificationType)
 
 
