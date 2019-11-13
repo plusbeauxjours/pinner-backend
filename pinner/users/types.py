fff0dc1d370cf63176e721dbad68f3c0dfa36198
plusbeauxjours <plusbeauxjours@gmail.com>
2019-11-13 12:09:26 +0900

    isRequestedCoffee

--- pinner/users/types.py
+++ pinner/users/types.py
@@ -23,6 +23,14 @@ class ProfileType(DjangoObjectType):
     trip_count = graphene.Int(source='trip_count')
     coffee_count = graphene.Int(source='coffee_count')
     is_self = graphene.Boolean()
+    is_requested_coffee = graphene.Boolean()
+
+    def is_requested_coffee(self, info):
+        user = info.context.user
+        if user.host.filter(status="requesting"):
+            return True
+        else:
+            return False
 
     def resolve_is_self(self, info):
         user = info.context.user
