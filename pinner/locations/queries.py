f1b42dede985d9560d9b23b0a60662fa35e2714d
plusbeauxjours <plusbeauxjours@gmail.com>
2019-12-03 18:10:10 +0900

    test

--- pinner/locations/queries.py
+++ pinner/locations/queries.py
@@ -559,9 +559,11 @@ def resolve_recommend_locations(self, info, **kwargs):
     if combined.count() < 10:
         print("less than 10")
         createdCities = models.City.objects.exclude(id=city.id).order_by('-created_at')[:5]
+        print("createdCities", createdCities, createdCities.count())
         combined = combined | createdCities
         print("combined1", combined, combined.count())
         likedCities = models.City.objects.exclude(id=city.id).order_by('likes')[:5]
+        print("likedCities", likedCities, likedCities.count())
         combined = combined | likedCities
         print("combined2", combined, combined.count())
         cities = get_locations_nearby_coords(city.latitude, city.longitude)
