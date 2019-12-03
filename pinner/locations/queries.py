58d069334d2bf5cba364672ec0be0add26bf94d5
plusbeauxjours <plusbeauxjours@gmail.com>
2019-12-03 19:21:32 +0900

    almost done

--- pinner/locations/queries.py
+++ pinner/locations/queries.py
@@ -515,7 +515,7 @@ def resolve_recommend_locations(self, info, **kwargs):
             gcd_formula,
             (latitude, longitude, latitude)
         )
-        qs = combined.annotate(distance=distance_raw_sql).order_by('-id').distinct('id').order_by('distance')
+        qs = combined.annotate(distance=distance_raw_sql).order_by('-id').distinct('id')
         return qs
 
     try:
